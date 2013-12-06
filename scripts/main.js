c2584053adfaca36754b6a5e726604922f36d636
Timm Allman <tallman@umass.edu>
2013-12-06 15:13:50 -0500

    Revamp a bunch of things. Layout game.js

--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-( function() {
-	'use strict';
-	var config = {}, backbone;
-	config.paths = {
-		'root' : '../..'
-	};
-} () );
---
+++ scripts/modules/map.js
@@ -0,0 +1,11 @@
+define([ 'jQuery', 'Backbone' ], function() {
+	Map = Backbone.Model.extend( {
+		initialize : function() {
+
+		}
+	} );
+
+	return {
+		Map : Map
+	};
+} );
