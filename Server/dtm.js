04030ac048ef6214c1458b8a46fcea6ff44a6655
Majiir Paktu <majiir@nabaal.net>
2013-12-06 12:04:02 -0500

    Chain DTM methods

--- Server/dtm.js
+++ Server/dtm.js
@@ -3,18 +3,23 @@ exports.start = function() {
 	return {
 		coins: function(userId, quantity) {
 			/* add quantity coins to userId’s coin count */
+			return this;
 		},
 		item: function(userId, itemId, quantity) {
 			/* add quantity itemIds for userId */
+			return this;
 		},
 		levelProgress: function(userId, levelId, score) {
 			/* update userId’s high score on levelId with score */
+			return this;
 		},
 		notification: function(userId, notification) {
 			/* add notification to userId’s queue */
+			return this;
 		},
 		receipt: function(receipt) {
 			/* will add the receipt, fails if the receipt already exists */	
+			return this;
 		},
 		commit: function() {
 			/* attempts to process transaction (returns true/false) */
