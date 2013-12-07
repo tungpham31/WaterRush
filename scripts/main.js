38df22dd9de902b9add127461175002fadc50939
Timm Allman <tallman@umass.edu>
2013-12-07 13:09:44 -0500

    begin work on power upds

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,6 @@
 /**
  * This is the entry point for the application. Should probably call Login or something
  */
-define( [ 'modules/login', 'modules/inventory', '_', '$' ], function( Login, inv ) {
-	var coin = inv.coin;
-	var c = new coin( 10 );
+define( [ 'modules/login', '_', '$' ], function( Login, inv ) {
+
 } );
