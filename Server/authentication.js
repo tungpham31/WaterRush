c0bf24ffa69ee85c14345180afa5b7746282df5f
Henry Merriam <hmerriam@umass.edu>
2013-12-07 18:10:17 -0500

    Unwrap batched data from payload

--- Server/authentication.js
+++ Server/authentication.js
@@ -4,6 +4,8 @@ module.exports = function (req, res, next) {
 	 * The req object should be augmented with information about the authenticated user.
 	 * If authentication fails, finish the response in this function and don't call next().
 	 */
+	req.body = req.body.data;
+
 	req.user = { id: 0 };
 	next();
 };
