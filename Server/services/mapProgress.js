1e14960afdcac26d81487f515a58fd9656042e40
GenEd <genmedwards@gmail.com>
2013-12-08 11:32:58 -0500

    Remove coin and lives checking from mapProgress.js

--- Server/services/mapProgress.js
+++ Server/services/mapProgress.js
@@ -12,45 +12,26 @@ exports.endpoints = {
 		 * the user. If this routine can fail, the returned object should indicate the
 		 * reason for failure.
 		 */
-        async.parallel({
-            highestLevel: function(callback){
-                drs.getScores(user, function(err, data){
-                    if (err){
-                        callback(err);
-                    }
-                    else{
-                        highestLevel = function(){
-                            /* find the highest level from the <data> */
-                            var maxLevel = -1;
-                            for (var level in data) {
-                              if (data.hasOwnProperty(level)) {
-                                if (maxLevel < level) {
-                                    maxLevel = level;
-                                }
-                              }
-                            }
-                            return maxLevel;
-                        };
-                        callback(null, highestLevel()); 
-                    }
-                });   
-            },
-            coins: function(callback){
-                drs.getCoins(user, callback);
-            },
-            lives: function(callback){
-                drs.getLives(user, callback);
-            },
-        },
-        function(err, results) {
+
+        drs.getScores(user, function(err, data){
             if (err){
-                // errrrrrrrrrrrrrrrrrr
-                // ask Henry later
                 done(err);
             }
             else{
-                done(results);
-            }    
-        });
+                highestLevel = function(){
+                    /* find the highest level from the <data> */
+                    var maxLevel = -1;
+                    for (var level in data) {
+                      if (data.hasOwnProperty(level)) {
+                        if (maxLevel < level) {
+                            maxLevel = level;
+                        }
+                      }
+                    }
+                    return maxLevel;
+                };
+                done(highestLevel()); 
+            }
+        });   
     },
 };
