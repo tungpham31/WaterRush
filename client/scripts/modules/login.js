a30453b585bc8e6b17a4583e4b9865d8d702339f
Henry Merriam <hmerriam@umass.edu>
2013-12-09 14:21:22 -0500

    Add login module skeleton back in

---
+++ client/scripts/modules/login.js
@@ -0,0 +1,11 @@
+define([], function() {
+
+	return {
+		/**
+		 * Returns a token which the server can use to retrieve and verify the user's identity.
+		 */
+		getToken: function () {
+			
+		},
+	};
+} );
